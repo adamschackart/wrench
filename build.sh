53cfa749a615f8ccac94a6a209a4533015ea99db
Adam Schackart <plainflavored@gmail.com>
2025-10-22 08:36:42 -0500

    GCC warning fix

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ if [ ! -d "wren" ]; then
     python3 -B wren/util/generate_amalgamation.py >> wren.c
 fi
 
-cc -g -c -o wren.o wren.c
+cc -g -fPIC -c -o wren.o wren.c
 
 cc -g -lm -ldl -I. -Iwren/src/include -o run_wren main.c wren.o
 cc -g -lm -ldl -lc++ -I. -Iwren/src/include -std=c++17 -fPIC -shared -o file.so file.cpp wren.o
